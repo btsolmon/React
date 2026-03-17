216b89f3464f4e526c92094de168eb5655ede2fe
btsolmon <btsolmon.mn@gmail.com>
2026-03-17 10:49:17 +0800

    first

---
+++ src/app/second/page.js
@@ -0,0 +1,141 @@
+import { ProductCard } from "../components/ProductCard";
+
+const page = () => {
+  return (
+    <div>
+      <ProductCard
+        name={"Э. Баасандорж"}
+        gender={"male"}
+        email={"baskaerka763@gmail.com"}
+        studentCode={"25BS65476"}
+      />
+      <ProductCard
+        name={"Т. Нямбаяр"}
+        gender={"male"}
+        email={"yunoxsama00@gmail.com"}
+        studentCode={"25LP7381"}
+      />
+      <ProductCard
+        name={"Л. Дөлгөөнзул"}
+        gender={"female"}
+        email={"dolgoonlkhagva@gmail.com"}
+        studentCode={"26BS86071"}
+      />
+      <ProductCard
+        name={"Г. Номин"}
+        gender={"female"}
+        email={"gnomin200408@gmail.com"}
+        studentCode={"25BS52373"}
+      />
+      <ProductCard
+        name={"Л. Няамочир"}
+        gender={"male"}
+        email={"nyamochir.lkhamkhvv@gmail.com"}
+        studentCode={"26BS98744"}
+      />
+      <ProductCard
+        name={"М. Азноров"}
+        gender={"female"}
+        email={"azaamunkhtn@gmail.com"}
+        studentCode={"25LP1286"}
+      />
+      <ProductCard
+        name={"Б. Билэгсайхан"}
+        gender={"male"}
+        email={"bbek3201@gmail.com"}
+        studentCode={"26BS83263"}
+      />
+      <ProductCard
+        name={"Ш. Батмагнай"}
+        gender={"male"}
+        email={"sbatmagnai25@gmail.com"}
+        studentCode={"25LP6073"}
+      />
+      <ProductCard
+        name={"Б. Чинзориг"}
+        gender={"male"}
+        email={"chinzoobatbold11@gmail.com"}
+        studentCode={"26BS13491"}
+      />
+      <ProductCard
+        name={"Б. Гаажадамба"}
+        gender={"male"}
+        email={"gaajadamba@gmail.com"}
+        studentCode={"26BS25985"}
+      />
+      <ProductCard
+        name={"О. Анударь"}
+        gender={"female"}
+        email={"anudariochiredene@gmail.com"}
+        studentCode={"26BS80134"}
+      />
+      <ProductCard
+        name={"Б. Мөнх Оргил"}
+        gender={"male"}
+        email={"suusiibat@gmail.com"}
+        studentCode={"26BS63531"}
+      />
+      <ProductCard
+        name={"К. Өрнүүн"}
+        gender={"male"}
+        email={"urnuun919@gmail.com"}
+        studentCode={"25BS21438"}
+      />
+      <ProductCard
+        name={"Т. Батхүлэг"}
+        gender={"male"}
+        email={"t.khuleg@gmail.com"}
+        studentCode={"26BS38301"}
+      />
+      <ProductCard
+        name={"Б. Цолмон"}
+        gender={"male"}
+        email={"btsolmon.mn@gmail.com"}
+        studentCode={"26BS11043"}
+      />
+      <ProductCard
+        name={"А. Билгүүнтөгс"}
+        gender={"male"}
+        email={"bilgntgsamarbayr@gmail.com"}
+        studentCode={"26BS10652"}
+      />
+      <ProductCard
+        name={"Ц. Номин Эрдэнэ"}
+        gender={"female"}
+        email={"nominerdenetsogtbaatar5@gmail.com"}
+        studentCode={"26BS60931"}
+      />
+      <ProductCard
+        name={"Э. Номин Эрдэнэ"}
+        gender={"female"}
+        email={"ne78693@gmail.com"}
+        studentCode={"26BS42529"}
+      />
+      <ProductCard
+        name={"А. Баярбилэг"}
+        gender={"male"}
+        email={"bayarbileg.am@gmail.com"}
+        studentCode={"26BS44296"}
+      />
+      <ProductCard
+        name={"М. Темүүлэн"}
+        gender={"male"}
+        email={"azeroth0017@gmail.com"}
+        studentCode={"26BS99683"}
+      />
+      <ProductCard
+        name={"Б. Баярцогт"}
+        gender={"male"}
+        email={"bayartsogt9998@gmail.com"}
+        studentCode={"26BS96881"}
+      />
+      <ProductCard
+        name={"Б. Мянганнаст"}
+        gender={"male"}
+        email={"myngannast.b@gmail.com"}
+        studentCode={"26BS98039"}
+      />
+    </div>
+  );
+};
+export default page;
